4e000b20d9347e2f6838bf0e2a215453b03ae291
volodyafrolov11 <frolovva.work@gmail.com>
2024-09-29 11:37:19 +0300

    add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+
+    const [count, setCount] = useState(0);
+
     return (
         <div>
-            Hello world
+            <h1>{count}</h1>
+            <button onClick={() => setCount(prev => prev + 1)}>increment</button>
+            <button onClick={() => setCount(prev => prev - 1)}>decrement</button>
         </div>
     );
 };
