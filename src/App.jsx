7221109f66ce8fc53e4ec4ac776e3ba49f748bd6
volodyafrolov11 <frolovva.work@gmail.com>
2024-09-29 11:40:07 +0300

    add styles for counter

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     const [count, setCount] = useState(0);
 
     return (
-        <div>
+        <div className="counter">
             <h1>{count}</h1>
             <button onClick={() => setCount(prev => prev + 1)}>increment</button>
             <button onClick={() => setCount(prev => prev - 1)}>decrement</button>
